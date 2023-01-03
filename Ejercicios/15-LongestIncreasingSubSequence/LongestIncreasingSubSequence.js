6c18bb933924b2abb86a6cf111fb2604f4614259
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-01-03 16:45:25 -0300

    15-Subsequence

--- Ejercicios/15-LongestIncreasingSubSequence/LongestIncreasingSubSequence.js
+++ Ejercicios/15-LongestIncreasingSubSequence/LongestIncreasingSubSequence.js
@@ -1,6 +1,18 @@
 function longestIncreasingSubsequence (nums) {
-  // Your code here:
-
+  const sequences = [];
+  let longest = 0;
+  for (const num of nums) {
+    const sequenceCopy = [...sequences]
+    sequences.push([num])
+    for (const seq of sequenceCopy) {
+      if(num > seq[seq.length -1]){
+        const newSequence = [...seq, num]
+        sequences.push(newSequence)
+        if(newSequence.length > longest) longest = newSequence.length
+      }      
+    }    
+  }
+  return longest;
 }
 
 module.exports = longestIncreasingSubsequence
