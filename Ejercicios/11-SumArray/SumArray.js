701772bbb64bbe1350cc64270d8ef0d6ba90a3ad
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-01-09 00:29:46 -0300

    SumArray

--- Ejercicios/11-SumArray/SumArray.js
+++ Ejercicios/11-SumArray/SumArray.js
@@ -1,6 +1,27 @@
 function SumArray (arr, n) {
-  // Your code here:
+  console.log(arr)
+  //                        s
+  //                        e
+  console.log(n)
+  // for (let i = 0; i < arr.length -1; i++) {
+  //   for (let j = i + 1; j < arr.length; j++) {
+  //     const sum = arr[i] + arr[j]
+  //     if(sum === n) return true
+  //   }    
+  // }
+  // return false
+  let start = 0;
+  let end = arr.length -1
+  while (start !== end) {
+    const sum = arr[start] + arr[end]
+    if(sum === n) return true
+    if(sum < n) start++
+    else end--    
+  }
+ return false
 
 }
+//    O(n**2)
 
 module.exports = SumArray
+
