003224c8887d2ae2405d60ca19f5d33f22adb08c
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-01-09 00:29:09 -0300

    MSumArray

--- Ejercicios/12-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/12-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,9 @@
-function mdArraySum (arr) {
-  // Your code here:
-
+function mdArraySum (arr, count = 0) { 
+  // for (const elem of arr) {
+  //   count = typeof elem === 'number' ?  count + elem : mdArraySum(elem, count)  
+  // }
+  // return count;
+  return arr.reduce((count, elem) => count + (typeof elem === 'number' ? elem : mdArraySum(elem, count)), 0)
 }
 
 module.exports = mdArraySum
